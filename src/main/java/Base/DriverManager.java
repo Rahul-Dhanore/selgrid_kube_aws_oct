316df021a475c18457806b82504b279a65010f02
rahuldhanore <rahul.t.dhanore@intimetec.com>
2025-04-08 11:13:20 +0530

    Exercise 1

--- src/main/java/Base/DriverManager.java
+++ src/main/java/Base/DriverManager.java
@@ -11,46 +11,45 @@ import java.net.URL;
 
 public class DriverManager {
 
-    private static ThreadLocal<WebDriver> threadLocalDriver= new ThreadLocal<>();
+    private static ThreadLocal<WebDriver> threadLocalDriver = new ThreadLocal<>();
 
-    public static WebDriver initDriver(){
-        String gridUrl=getGridUrl();
-        if(!(gridUrl==null || gridUrl.isEmpty())){
+    public static WebDriver initDriver() {
+        String gridUrl = getGridUrl();
+        if (!(gridUrl == null || gridUrl.isEmpty())) {
             try {
-                threadLocalDriver.set(new RemoteWebDriver(new URL(gridUrl),getChromeOptions()));
+                threadLocalDriver.set(new RemoteWebDriver(new URL(gridUrl), getChromeOptions()));
             } catch (MalformedURLException e) {
                 throw new RuntimeException(e);
             }
-        }
-        else {
+        } else {
             threadLocalDriver.set(new ChromeDriver(getChromeOptions()));
         }
         threadLocalDriver.get().manage().window().maximize();
         return threadLocalDriver.get();
     }
 
-    public static ChromeOptions getChromeOptions(){
+    public static ChromeOptions getChromeOptions() {
         ChromeOptions chromeOptions = new ChromeOptions();
 //        chromeOptions.addArguments("--no-sandbox");
 //        chromeOptions.addArguments("--disable-dev-shm-usage");
-//        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--headless");
         return chromeOptions;
     }
 
-    private static String getGridUrl(){
-        String gridUrl=null;
-        String gridUrlProp=PropertyHandler.getProperty(PropertyHandler.SELGRID_URL_KEY);
-        if(gridUrlProp!=null && !gridUrlProp.isEmpty()){
-            gridUrl =  String.format("http://%s:4444",gridUrlProp);
+    private static String getGridUrl() {
+        String gridUrl = null;
+        String gridUrlProp = PropertyHandler.getProperty(PropertyHandler.SELGRID_URL_KEY);
+        if (gridUrlProp != null && !gridUrlProp.isEmpty()) {
+            gridUrl = String.format("http://%s:4444", gridUrlProp);
         }
         return gridUrl;
     }
 
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
         return threadLocalDriver.get();
     }
 
-    public static void quitDriver(){
+    public static void quitDriver() {
         threadLocalDriver.get().quit();
         threadLocalDriver.remove();
     }
